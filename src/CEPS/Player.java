8fb034a26fbafd8cf53dc71b6ceb6f4b614a3554
Jordan <j_leble@yahoo.com>
2017-08-08 09:51:11 -0700

    Fixed method name

--- src/CEPS/Player.java
+++ src/CEPS/Player.java
@@ -11,7 +11,7 @@ public class Player {
 	public Player(int secret, int players) {
 		myPoly = Polynomial.randPoly(secret, (players / 2) + 1);
 		//myPoly = Polynomial.randPoly(7, 6);
-		myShares = myPoly.compute_shares(players);
+		myShares = myPoly.computeShares(players);
 		
 	}
 }
