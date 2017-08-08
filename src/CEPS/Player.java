1ef276c6920c8532eff0b34c2fb6d7441c514587
Jordan <j_leble@yahoo.com>
2017-08-08 09:55:19 -0700

    Fixed degree

--- src/CEPS/Player.java
+++ src/CEPS/Player.java
@@ -9,9 +9,7 @@ public class Player {
 	
 	
 	public Player(int secret, int players) {
-		myPoly = Polynomial.randPoly(secret, (players / 2) + 1);
-		//myPoly = Polynomial.randPoly(7, 6);
+		myPoly = Polynomial.randPoly(secret, players / 2);
 		myShares = myPoly.computeShares(players);
-		
 	}
 }
