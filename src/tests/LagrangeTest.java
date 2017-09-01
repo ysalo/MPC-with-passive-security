0b2be416dff99e5924cf72e0812684477e7d08d3
ysalo <slav27@gmail.com>
2017-08-31 18:02:05 -0700

    move jars to lib

--- src/tests/LagrangeTest.java
+++ src/tests/LagrangeTest.java
@@ -1,5 +1,19 @@
 package tests;
 
+import org.junit.Test;
+
+/**
+ * 
+ * Unit tests for class Lagrange.
+ * 
+ * @author Yaro Salo
+ * @version 1
+ */
 public class LagrangeTest {
+    
+    @Test 
+    public void testInterpolate() {
+        
+    }
 
 }
